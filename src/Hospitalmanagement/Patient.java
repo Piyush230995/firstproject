3e2a6e1ce38b27463f33d0d08a648aa5aaab0220
Piyush230995 <piyushnair223.com>
2026-04-28 13:38:17 +0530

    viewpatients

--- src/Hospitalmanagement/Patient.java
+++ src/Hospitalmanagement/Patient.java
@@ -2,6 +2,7 @@ package Hospitalmanagement;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -42,6 +43,21 @@ public class Patient{
 
 
     public void viewPatients(){
+        try{
+            String query = "SELECT * FROM patients";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String gender = rs.getString("gender");
+                System.out.printf("|%-10d|%-10s|%-10d|%-10s|", id, name, age, gender);
+            }
+
+        } catch (SQLException e){
+        e.printStackTrace();
+        }
 
     }
 
